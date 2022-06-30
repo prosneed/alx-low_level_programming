17552f015d637ab87a93bd43061592cf0b1861ec
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-06-30 13:15:59 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 /**
- *main - entry point
- *
- *returns 0 after printing the function
- */
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
 }
