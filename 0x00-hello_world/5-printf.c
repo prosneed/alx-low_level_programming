0aeb3a64d0958556e004dcd89f54f146c36b3e36
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-06-30 13:31:52 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
- *
- * Return 0 after printing the function
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after program execution
  */
 int main(void)
 {
