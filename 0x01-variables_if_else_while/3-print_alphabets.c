f69a30254e272c580803896519009a4018eb9a51
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-01 12:32:22 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 always 0 (success/correct)
+ */
+int main(void)
+{
+	char i,j;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
