59fa146f87fde5506a0fbfb2f7154379a3b285b5
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-01 13:25:40 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 always 0 (always/success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
