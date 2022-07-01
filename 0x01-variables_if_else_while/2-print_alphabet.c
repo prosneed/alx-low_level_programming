40e13cf055c8bbc91e3fed7d17a56f1a8aede1e4
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-01 11:36:56 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always 0 (Success/Correct)
+ */
+int main(void)
+{
+	char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+	return (0);
+}
