df41f0a5928099000a0e9097e0d5022cb7fabb35
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-01 13:41:37 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 always 0 (success/correct)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar('i');
+	}
+	putchar('\n');
+	return (0);
+}
