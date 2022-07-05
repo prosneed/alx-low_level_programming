ea58c41b271b9ac5f374a909eed00ef73c0b0367
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-05 15:00:37 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @int: passes the int number
+ * Return: return the remainder always 1
+ */
+
+int print_last_digit(int)
+{
+	int x;
+
+	return (x % 10);
+
+}
