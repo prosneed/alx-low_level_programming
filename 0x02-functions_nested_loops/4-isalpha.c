1a3e4ea35c5a0944dd45ad3572876cb1b5d0ff30
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-05 13:41:53 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks if letter
+ * @c: contains value to be compared
+ * Return: 1 if letter and 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c >= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
