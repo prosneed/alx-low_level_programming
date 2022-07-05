d66fd277a0c8135d94b46b953664be85d9c8d3a1
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-05 14:38:43 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,14 +2,14 @@
 /**
  * print_sign - prints the sign of a number
  * @n: contains value to be compared
- * Return: 1 if positive or 0 if zero or -1 if negative
+ * Return: 1 always success
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_puthcar('+');
 		return (1);
 	}
 	else if (n == 0)
