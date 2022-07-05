c576a1e8df435adb1588961c2c810ed0833090b0
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-05 14:22:48 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,24 +10,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('1');
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('0');
-
+		return (0);
 	}
 	else
 	{
-		_putchar('-1');
-		_putchar(',');
-		_putchar(' ');
 		_putchar('-');
-		_putchar('1');
+		return (-1);
 	}
 }
