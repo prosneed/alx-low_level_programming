e64dffff7cf956d59f349efb29171ad0cdaa1d38
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-07 07:08:24 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: to be tracked
+ * @b: to be tracked
+ * Return: 0 if successful
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
