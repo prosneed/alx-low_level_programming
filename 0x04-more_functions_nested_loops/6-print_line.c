c5c0832559bac0963188e3df9799b693ac7fa38a
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-07 09:29:14 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: parameter to be tracked
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar("_");
+	}
+}
