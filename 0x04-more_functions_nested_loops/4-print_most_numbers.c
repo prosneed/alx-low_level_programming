edda3ac5749724839a9935c10b3ae68efbbac1a1
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-07 07:47:06 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,17 +11,9 @@ void print_most_numbers(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i == 50)
-		{
-			continue;
-		}
-		else if (i == 52)
-		{
-			continue;
-		}
-		else
+		if (i != 50 && i != 52)
 		{
 			_putchar(i);
 		}
-		_putchar('\n');
+	_putchar('\n');
 }
