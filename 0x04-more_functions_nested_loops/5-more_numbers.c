271ff0a40ddba07d455b8fb7a42d496ed5503130
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-07 09:17:43 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
+
 /**
- * more_numbers - prints 10 times from 0 to 14
- *
- * Return: returns nothing
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  */
 
 void more_numbers(void)
