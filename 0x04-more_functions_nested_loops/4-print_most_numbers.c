3d88a7e2e8a15109f4468c4b02ecd44a0ad51b45
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-07 07:24:57 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints most of the numbers
+ *
+ * Retrun: returns nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+}
