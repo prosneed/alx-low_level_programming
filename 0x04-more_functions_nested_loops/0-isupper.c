344246fd4ae969a7778e7c0f09d82a49b0df9b36
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-07 06:37:58 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * int _isupper - checks if uppercase or not
+ * @c - accepts c as a parameter
+ * Return: 1 if upper or 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <=96)
+	{
+		return (10);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
