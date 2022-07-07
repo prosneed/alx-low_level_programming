bfd847a913ff8878ad8d090a8a85529b6139f1c0
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-07 15:19:56 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle using hash sign
+ * @size: parameter is being tracked
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+	int x;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(32);
+			}
+			for (x = 0; x < i; x++)
+			{
+				_putchar(35);
+			}
+		}
+	}
+	_putchar(10);
+}
