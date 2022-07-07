52ee14664c626de0efec54caff2e1973abb71b53
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-07 13:03:40 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,22 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(32);
-		}
-		_putchar(92);
+		_putchar(36);
 		_putchar(10);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar(36);
+			_putchar(10);
+		}
 	}
 }
