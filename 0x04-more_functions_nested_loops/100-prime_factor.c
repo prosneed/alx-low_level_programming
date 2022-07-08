493eb49f66b15f72a1d9c72f32becb8a055a733e
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-07 17:35:37 -0700

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,39 +1,3 @@
-/*
- * #include <stdio.h>
-#include <stdlib.h>
-**
- * main - entry point
- *
- * Return: 0 if successful
- *
-
-int main(void)
-{
-	long int num = 612852475143;
-	int i, j;
-	long int largest;
-
-	for (i = 1; i <= num / 2; i++)
-	{
-		int counter = 0;
-
-		for (j = 1; j <= i; j++)
-		{
-			if (i % j == 0)
-			{
-				counter = counter + 1;
-			}
-		}
-		if (counter == 2)
-		{
-			largest = i;
-		}
-	}
-	printf("%ld\n", largest);
-	return (0);
-}
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
