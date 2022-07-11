12798e9e2a9c5e45a2ffc4c37dff9b692523cbf0
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-11 13:15:33 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strlen - displays the length of a string
+ * @s: a string pointer to a char
+ * Return: 0 if successful
+ */
+
+int _strlen(char *s)
+{
+	int j, length = 0;
+
+	/**
+	*for (i = 0; i != '\0'; i++)
+	*{
+	*	length++;
+	*}
+	*/
+
+	for (j = 0; j = '\0'; j++)
+	{
+		s[i];
+		lenght++;
+	}
+	return (length);
+}
