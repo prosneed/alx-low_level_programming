64bcfbb3c5e74e5d7725dfd91c9babbb1d408db6
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-11 14:49:10 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: pointer to a string
+ * Return: returns nothing
+ */
+
+void print_rev(char *s)
+{
+	int i = 0, length = 0, j;
+
+	while (s[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	for (j = length; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
