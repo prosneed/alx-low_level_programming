124ca4851ea0d3e7e68db43a76ca949d97e511f0
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-13 14:14:15 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "man.h"
+/**
+ * _strncpy - copies string one from the other
+ * @dest: pointer to char
+ * @src: pointer to char
+ * @n: size
+ * Return: 0 if successful
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+		int i = 0, k = 0;
+
+	while (n > k)
+	{
+		if (src[k] == '\0')
+		{
+			for (; k < n; k++)
+			{
+				dest[i] = '\0';
+				i++;
+			}
+		}
+		else
+		{
+			dest[i] = src[k];
+			k++;
+			i++;
+		}
+	}
+
+	return (dest);
+}
