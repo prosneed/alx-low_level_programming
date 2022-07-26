df079dfcb4ffd7a6f9dd627393bd6883ff582f8b
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-07-26 12:23:30 -0700

    task 0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strdup - dupulicates str
+ * @str: accepts str and returns memory location
+ * Return: returns pointer to memory location
+ */
+char *_strdup(char *str);
+{
+	str = malloc(sizeof(*str));
+	return (str);
+}
