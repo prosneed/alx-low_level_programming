9283b38b716e3560f831300e80ae06f70deadf10
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-08-04 15:35:56 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,14 +8,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum, i;
+	unsigned int sum = 0, i;
 
 	va_list list;
-
 	va_start(list, n);
-	for (i = 0; i < n; i++)
+	for (i = 1; i <= n; i++)
 	{
-		sum = va_arg(list, const unsigned int);
+		sum += va_arg(list, unsigned int);
 	}
 	va_end(list);
 	return (sum);
