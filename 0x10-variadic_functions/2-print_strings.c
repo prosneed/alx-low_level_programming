0fa863cf04c6e9117bdc3e7cf4cef69ca7b3761d
Nebiyou Yemam <nebiyouyemam@gmail.com>
2022-08-04 17:51:42 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,14 +15,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(l, n);
 	for (i = 0; i < n; i++)
 	{
-		/*
-		 *if (va_arg(ls, int *) == NULL)
-		 *{
-		 *	printf("%s%s", "nil", (separator && i != n - 1) ? separator : "");
-		 *	continue;
-		 *}
-		 */
-	printf("%s%s", va_arg(l, char *), (separator && i != n - 1) ? separator : "");
+		if (va_arg(l, char *) == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s%s", va_arg(l, char *), (separator && i != n - 1) ? separator : "");
+		}
 	}
 	va_end(l);
 	printf("\n");
